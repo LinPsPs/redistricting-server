bdfc252335314f7bb10a94c6ca99668cbdf4a501
LinPsPs <haolin.yu@outlook.com>
2021-04-21 10:08:10 -0400

    Initial commit

---
+++ src/main/java/com/sbu/redistrictingserver/RedistrictingServerApplication.java
@@ -0,0 +1,13 @@
+package com.sbu.redistrictingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedistrictingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedistrictingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbu/redistrictingserver/RedistrictingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbu.redistrictingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedistrictingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
